6e4e77dc1bd28a48ed43ef8d2668940e6aaebc45
ankit <ankitprasad.id@gmail.com>
2021-11-10 03:31:44 +0530

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("nice!!");
+    Person person = new Person("Ankit");
+    System.out.println(person.getName());
+  }
+}
